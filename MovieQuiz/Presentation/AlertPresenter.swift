a26312f597f283874e5ffa310d81df335c390053
Alexander Khasianevich <hasman32320@gmail.com>
2023-06-25 23:47:46 +0300

    Ended 5-th sprint

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,24 @@
+import Foundation
+import UIKit
+
+class AlertPresenter: AlertPresenterProtocol {
+    private weak var viewController: UIViewController?
+    
+    init(viewController: UIViewController? = nil) {
+        self.viewController = viewController
+    }
+    
+    func showAlert(_ model: AlertModel) {
+        let alert = UIAlertController(title: model.title,
+                                      message: model.message,
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: model.buttonText,
+                                   style: .default) { _ in
+            model.completion()
+        }
+        
+        alert.addAction(action)
+        viewController?.present(alert, animated: true)
+    }
+}
